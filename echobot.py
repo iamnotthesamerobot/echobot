97b2faead0755d4fbbbe66b92ee9b4b65cfa4111
iamnotthesamerobot <42997264+iamnotthesamerobot@users.noreply.github.com>
2018-09-18 17:09:09 +0300

    Update echobot.py
    token minus

--- echobot.py
+++ echobot.py
@@ -5,7 +5,7 @@
 
 import telebot
 
-echobot = telebot.TeleBot('574405257:AAFdIn5ko9ZoNLgQZZkGnzGncZIFvohjEqo')
+echobot = telebot.TeleBot('token')
 
 @echobot.message_handler(content_types=["text"])
 def repeat_all_messages(message): 
